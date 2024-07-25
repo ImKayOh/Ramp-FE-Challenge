89e8a1da6b85cccd53659925ccdbb64baf210156
Keith O'Donnell <49801759+ImKayOh@users.noreply.github.com>
2024-07-25 15:32:07 +0000

    Bug Fix One

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -1,16 +1,21 @@
-import classNames from "classnames"
-import { useRef } from "react"
-import { InputCheckboxComponent } from "./types"
+import classNames from "classnames";
+import { useRef } from "react";
+import { InputCheckboxComponent } from "./types";
 
-export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+export const InputCheckbox: InputCheckboxComponent = ({
+  id,
+  checked = false,
+  disabled,
+  onChange,
+}) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`);
 
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
-          "RampInputCheckbox--label-disabled": disabled,
+          "RampInputCheckbox--label-disabled": disabled /*BUG FIX TWO*/,
         })}
       />
       <input
@@ -22,5 +27,5 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         onChange={() => onChange(!checked)}
       />
     </div>
-  )
-}
+  );
+};
